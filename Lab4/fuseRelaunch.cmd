99440a7706ae9e14220e24494b3cc06a764e7e98
Hans Tananda <hanstananda@gmail.com>
2018-11-06 18:43:06 +0800

    added resources

---
+++ Lab4/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "E:/Lab4/TestbenchLab4_isim_beh.exe" -prj "E:/Lab4/TestbenchLab4_beh.prj" "work.TestbenchLab4" "work.glbl" 
